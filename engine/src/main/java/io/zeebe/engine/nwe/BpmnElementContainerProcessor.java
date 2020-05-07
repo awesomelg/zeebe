3958a4d494744bad3c5b13d69ab36922431deaa8
Philipp Ossler <philipp.ossler@gmail.com>
2020-05-07 07:16:59 +0200

    chore(engine): add JavaDoc for processor interfaces

--- engine/src/main/java/io/zeebe/engine/nwe/BpmnElementContainerProcessor.java
+++ engine/src/main/java/io/zeebe/engine/nwe/BpmnElementContainerProcessor.java
@@ -9,14 +9,40 @@ package io.zeebe.engine.nwe;
 
 import io.zeebe.engine.processor.workflow.deployment.model.element.ExecutableFlowElement;
 
+/**
+ * The business logic of an BPMN element container (e.g. a sub-process).
+ *
+ * <p>The execution of an element is divided into multiple steps that represents the lifecycle of
+ * the element. Each step defines a set of actions that can be performed in this step. The
+ * transition to the next step must be triggered explicitly in the current step.
+ *
+ * @param <T> the type that represents the BPMN element
+ */
 public interface BpmnElementContainerProcessor<T extends ExecutableFlowElement>
     extends BpmnElementProcessor<T> {
 
-  void onChildCompleted(
+  /**
+   * A child element is completed. Leave the element container if it has no more active child
+   * elements.
+   *
+   * @param element the instance of the BPMN element container
+   * @param flowScopeContext workflow instance-related data of the element container
+   * @param childContext workflow instance-related data of the child element that is completed
+   */
+  public abstract void onChildCompleted(
       final T element,
       final BpmnElementContext flowScopeContext,
       final BpmnElementContext childContext);
 
-  void onChildTerminated(
+  /**
+   * A child element is terminated. Terminate the element container if it has no more active child
+   * elements. Or, continue with the interrupting event sub-process that was triggered and caused
+   * the termination.
+   *
+   * @param element the instance of the BPMN element container
+   * @param flowScopeContext workflow instance-related data of the element container
+   * @param childContext workflow instance-related data of the child element that is terminated
+   */
+  public abstract void onChildTerminated(
       T element, BpmnElementContext flowScopeContext, BpmnElementContext childContext);
 }
